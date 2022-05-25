6d99cb6787b6e0b6dbe18f3699591f42f36cb9ea
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-25 23:35:55 +0700

    first commit

---
+++ lib/model/travel_photo.dart
@@ -0,0 +1,13 @@
+class TravelPhoto {
+  const TravelPhoto({
+    required this.backImage,
+    required this.frontImage,
+    required this.name,
+    required this.photos,
+  });
+
+  final String backImage;
+  final String frontImage;
+  final String name;
+  final int photos;
+}
