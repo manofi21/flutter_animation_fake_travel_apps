fbfdca8c7e9432ccc2e6dd29ab9ba4cf29cb3d3e
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-26 21:57:32 +0700

    create travel_photo_details

---
+++ lib/view/travel_photo_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:travel_guide_apps_animation/model/travel_photo.dart';
+
+class TravelPhotoDetails extends StatelessWidget {
+  const TravelPhotoDetails({
+    Key? key,
+    required this.travelPhoto,
+  }) : super(key: key);
+  final TravelPhoto travelPhoto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
