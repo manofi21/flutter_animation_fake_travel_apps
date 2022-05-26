e1039567daf88b4876ed0262672cc81996d3a45a
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-26 23:14:33 +0700

    create travel_photo_list_item.dart

---
+++ lib/view/travel_photo_list_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TravelPhotoListItem extends StatelessWidget {
+  const TravelPhotoListItem({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
