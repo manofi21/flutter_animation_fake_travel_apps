c7206b5cef27e7324f5a39cdb0e008f19d756a1b
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-26 23:24:10 +0700

    create fake_review

---
+++ lib/view/fake_review.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FakeReview extends StatelessWidget {
+  const FakeReview({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
