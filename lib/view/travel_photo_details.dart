d09b2bd801aa87f189b4ca4b1a8693ea97c56245
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-27 01:17:11 +0700

    create header image view

--- lib/view/travel_photo_details.dart
+++ lib/view/travel_photo_details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_guide_apps_animation/model/travel_photo.dart';
 
-class TravelPhotoDetails extends StatelessWidget {
+class TravelPhotoDetails extends StatefulWidget {
   const TravelPhotoDetails({
     Key? key,
     required this.travelPhoto,
@@ -9,7 +9,72 @@ class TravelPhotoDetails extends StatelessWidget {
   final TravelPhoto travelPhoto;
 
   @override
+  State<TravelPhotoDetails> createState() => _TravelPhotoDetailsState();
+}
+
+class _TravelPhotoDetailsState extends State<TravelPhotoDetails>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final _movement = -100.0;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, _) {
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned.fill(
+              left: _movement * _controller.value,
+              right: _movement * (1 - _controller.value),
+              child: Image.asset(
+                widget.travelPhoto.backImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: 10,
+              right: 10,
+              height: 100,
+              child: FittedBox(
+                child: Text(
+                  widget.travelPhoto.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              left: _movement * _controller.value,
+              right: _movement * (1 - _controller.value),
+              child: Image.asset(
+                widget.travelPhoto.frontImage,
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
